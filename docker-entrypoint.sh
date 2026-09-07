e8f86a40eed3c81f6bcf14f492dc5e27f69fa8a2
Pablo-M-Santos <pablomoreirasantos.hp@gmail.com>
2026-09-06 23:48:45 -0300

    refactor: Environment settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,10 +14,22 @@ until rabbitmq-diagnostics -q ping; do
     sleep 2
 done
 
-echo "RabbitMQ iniciado com sucesso!"
+echo "Node RabbitMQ disponível."
+
+echo "Aguardando aplicação RabbitMQ..."
+
+until rabbitmqctl status >/dev/null 2>&1; do
+    sleep 2
+done
+
+echo "Aplicação RabbitMQ disponível!"
+
+echo "========================================"
+echo "Configurando usuário RabbitMQ..."
+echo "========================================"
 
 rabbitmqctl delete_user admin 2>/dev/null || true
-rabbitmqctl add_user admin admin
+rabbitmqctl add_user admin "${RABBITMQ_PASSWORD:-admin}"
 rabbitmqctl set_user_tags admin administrator
 rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
 
